2a764860325b28974040fbd9f43c1eecedf71ea8
youssef <youssef.abdelmak@gmail.com>
2022-12-04 13:33:22 -0700

    some changes

--- src/Helpers/RequestHelpers.js
+++ src/Helpers/RequestHelpers.js
@@ -4,11 +4,13 @@ class RequestHelpers{
 
     //returns the first two options of the tree USA or Canada
     getInitialOptions(){
+        //return axios.get("https://legalbriefserver.herokuapp.com/optionsInitial");
         return axios.get("http://localhost:3002/optionsInitial");
     }
 
     //returns the children of the selected option
     getNextOption(parent){
+        //var request = "https://legalbriefserver.herokuapp.com/nextOptionSet" + parent;
         var request = "http://localhost:3002/nextOptionSet" + parent;
         return axios.get(request);
     }
