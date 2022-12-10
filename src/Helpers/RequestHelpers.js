619d149b28ea60b5e5fef84b1652f6a773af8eb8
youssef <youssef.abdelmak@gmail.com>
2022-12-10 14:14:48 -0700

    Update RequestHelpers.js

--- src/Helpers/RequestHelpers.js
+++ src/Helpers/RequestHelpers.js
@@ -4,14 +4,14 @@ class RequestHelpers{
 
     //returns the first two options of the tree USA or Canada
     getInitialOptions(){
-        //return axios.get("https://legalbriefserver.herokuapp.com/optionsInitial");
-        return axios.get("http://localhost:3002/optionsInitial");
+        return axios.get("https://legalbriefserver.herokuapp.com/optionsInitial");
+        //return axios.get("http://localhost:3000/optionsInitial");
     }
 
     //returns the children of the selected option
     getNextOption(parent){
-        //var request = "https://legalbriefserver.herokuapp.com/nextOptionSet" + parent;
-        var request = "http://localhost:3002/nextOptionSet" + parent;
+        var request = "https://legalbriefserver.herokuapp.com/nextOptionSet" + parent;
+        //var request = "http://localhost:3000/nextOptionSet" + parent;
         return axios.get(request);
     }
 }
