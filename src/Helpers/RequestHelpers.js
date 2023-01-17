367ea476f9eecf4950a6ee802fe0bfb09a3cdc73
youssef <youssef.abdelmak@gmail.com>
2023-01-17 14:54:04 -0700

    switching connection to host

--- src/Helpers/RequestHelpers.js
+++ src/Helpers/RequestHelpers.js
@@ -4,22 +4,22 @@ class RequestHelpers{
 
     //returns the first two options of the tree USA or Canada
     getInitialOptions(){
-        //return axios.get("https://legalbriefserver.herokuapp.com/optionsInitial");
-        return axios.get("http://localhost:3000/optionsInitial");
+        return axios.get("https://legalbriefserver.herokuapp.com/optionsInitial");
+        //return axios.get("http://localhost:3000/optionsInitial");
     }
 
     //returns the children of the selected option
     getNextOption(parent){
-        //var request = "https://legalbriefserver.herokuapp.com/nextOptionSet" + parent;
+        var request = "https://legalbriefserver.herokuapp.com/nextOptionSet" + parent;
         console.log(parent);
-        var request = "http://localhost:3000/nextOptionSet/" + parent;
+        //var request = "http://localhost:3000/nextOptionSet/" + parent;
         return axios.get(request);
     }
 
     sendEmail(content){
         console.log(content);
-        //axios.post('https://legalbriefserver.herokuapp.com/send-email', content)
-        axios.post('http://localhost:3000/send-email', content)
+        axios.post('https://legalbriefserver.herokuapp.com/send-email', content)
+        //axios.post('http://localhost:3000/send-email', content)
         .then(response => {
             console.log(response.data);
         }).catch(
