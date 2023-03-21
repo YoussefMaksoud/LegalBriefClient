48ad549a9e9d0af0d8e62029f165fe1b6e043ca5
youssef <youssef.abdelmak@gmail.com>
2023-03-21 12:24:48 -0600

    Update RequestHelpers.js

--- src/Helpers/RequestHelpers.js
+++ src/Helpers/RequestHelpers.js
@@ -4,13 +4,13 @@ class RequestHelpers{
 
     //returns the first two options of the tree USA or Canada
     getInitialOptions(){
-        return axios.get("https://legalbriefserver.herokuapp.com/optionsInitial");
+        return axios.get("https://legalbrief.herokuapp.com/optionsInitial");
         //return axios.get("http://localhost:8080/optionsInitial");
     }
 
     //returns the children of the selected option
     getNextOption(parent){
-        var request = "https://legalbriefserver.herokuapp.com/nextOptionSet/" + parent;
+        var request = "https://legalbrief.herokuapp.com/nextOptionSet/" + parent;
         console.log(parent);
         //var request = "http://localhost:8080/nextOptionSet/" + parent;
         return axios.get(request);
@@ -18,7 +18,7 @@ class RequestHelpers{
 
     sendEmail(content){
         console.log(content);
-        axios.post('https://legalbriefserver.herokuapp.com/send-email', content)
+        axios.post('https://legalbrief.herokuapp.com/send-email', content)
         //axios.post('http://localhost:8080/send-email', content)
         .then(response => {
             console.log(response.data);
